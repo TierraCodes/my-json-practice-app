74facf7bfac7ba7fd6c076e9664eb8b85b32dcf1
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2024-11-01 11:57:30 -0400

    Fully functional!!

--- app/page.tsx
+++ app/page.tsx
@@ -15,25 +15,24 @@ export default function Home() {
       })
   }, []);
 
+  let [selectedElement, setSelectedElement]=useState(-1)
+
   let gameList = games.map((game) => 
-    <li key={game.id} onClick={() => handleClick(game.releaseDate, game.price)}>{game.name}</li>);
-
-  const handleClick = (date, cost) => {
-    isClicked = true
-    releaseDate = date
-    price = cost
-  }
-
-  let isClicked = false
-  let price = '0'
-  let releaseDate = 'NA'
-
+    <li key={game.id} 
+        onClick={() => setSelectedElement(game.id)}>
+        {game.name}
+        <span style = {{display: selectedElement == game.id ? "block": "none"}}>
+          <p>Release Date: {game.releaseDate}</p>
+          <p>Price: {game.price}</p>
+        </span>
+    </li>);
+    
   return (
     <div>
       <h1>List of Cool Games:</h1>
       <aside>Click a title for more info!</aside>
       <ul>
-        {isClicked ? gameList + ` - Release Date: ${releaseDate} - Price: ${price}`: gameList}
+        {gameList}
       </ul>
     </div>
   );
